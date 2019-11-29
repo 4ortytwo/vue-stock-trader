2d379467154ad56a4ba6b704ad162cc9bec621fa
aburgan <44406493+4ortytwo@users.noreply.github.com>
2019-11-30 00:52:58 +0100

    add store and routes

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,40 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
+  state: {
+    stocks: [
+      {
+        name: "SAMSUNG",
+        currentPrice: 100
+      },
+      {
+        name: "GOOGLE",
+        currentPrice: 100
+      },
+      {
+        name: "APPLE",
+        currentPrice: 100
+      },
+      {
+        name: "TESLA",
+        currentPrice: 100
+      }
+    ],
+    ownedStocks: []
+  },
+  getters: {
+    stocks: state => {
+      return state.stocks;
+    },
+    ownedStocks: state => {
+      return state.ownedStocks;
+    }
+  },
+  mutations: {
+    buyStock: (state, payload) => {
+      state.ownedStocks.push(payload);
+    }
+  },
   actions: {},
   modules: {}
 });
