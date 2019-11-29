0ac1c5502160b94388577fc0a4e4382e0808fecf
aburgan <44406493+4ortytwo@users.noreply.github.com>
2019-11-30 00:55:21 +0100

    adjust App and Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,13 @@
 <template>
-  <div class="home"></div>
+  <div class="home border border-dark rounded-lg p-4">
+    <h3>Trade or View Your Portfolio</h3>
+    <p>You may Save &amp; Load your Data</p>
+    <p>Click 'End Day' to begin a new Day!</p>
+    <hr />
+    <p>
+      <strong>Your Funds: $10,000</strong>
+    </p>
+  </div>
 </template>
 
 <script>
